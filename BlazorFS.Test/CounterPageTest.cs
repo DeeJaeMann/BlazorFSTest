cdea94c2f2912548bf744b43243a2d417a24fd17
Dee J. Mann <dee.j.mann@hotmail.com>
2024-09-26 12:17:17 -0400

    Added Counter Header Test Added comments to Counter Test on intermittant failures Removed default UnitTest Changes to be committed: 	modified:   CounterPageTest.cs 	deleted:    UnitTest1.cs 	modified:   ../BlazorFS/Components/Pages/Counter.razor

--- BlazorFS.Test/CounterPageTest.cs
+++ BlazorFS.Test/CounterPageTest.cs
@@ -9,16 +9,27 @@ namespace BlazorFS.Test;
 public class CounterTests : BlazorTest
 {
     [Test]
+    public async Task Counter_Page_Has_Heading()
+    {
+        // Uncomment this when using headed mode to debug test
+        //await Page.PauseAsync();
+
+        await Page.GotoAsync(RootUri.AbsoluteUri, new() { WaitUntil = WaitUntilState.NetworkIdle });
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Counter" }).ClickAsync();
+        await Expect(Page.GetByRole(AriaRole.Heading)).ToHaveTextAsync("Counter");
+    }
+    [Test]
     public async Task Count_Increments_When_Button_Is_Clicked()
     {
+        // Uncomment this when using headed mode to debug test
         //await Page.PauseAsync();
 
         await Page.GotoAsync(RootUri.AbsoluteUri, new() { WaitUntil = WaitUntilState.NetworkIdle });
         await Page.GetByRole(AriaRole.Link, new() { Name = "Counter" }).ClickAsync();
         await Page.GetByRole(AriaRole.Button, new() { Name = "Click me" }).ClickAsync();
-        // The test was checking before the button had updated.
-        // The correct style of call needs to be made here
-        await Page.PauseAsync();
+        // This is intermittantly failing
+        // It seems that there may need to be an increase in the delay before checking
+        // adding a Pause.Async() only passes some of the time so that is not the correct solution
         await Expect(Page.GetByRole(AriaRole.Status)).ToHaveTextAsync("Current count: 1");
     }
 }
